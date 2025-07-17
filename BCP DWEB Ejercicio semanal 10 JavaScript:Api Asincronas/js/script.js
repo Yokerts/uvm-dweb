c0e715ab09d31f41940faa005b76edafd0d25bda
acrescodev <acresco.dev@gmail.com>
2025-07-17 14:41:51 -0600

    Se agrego el ejercicio 10 JavaScript y el consumo de APIs Asincronas

---
+++ BCP DWEB Ejercicio semanal 10 JavaScript:Api Asincronas/js/script.js
@@ -0,0 +1,20 @@
+async function buscarProducto(numberPage) {
+     return new Promise((resolve, reject) => {
+        fetch(`https://fakestoreapi.com/products/${numberPage}`)
+        .then((response) => {
+            // `response` es un objeto Response que incluye código de estado y métodos para leer el cuerpo
+            if (!response.ok) {
+                // Si el servidor responde con un error (por ejemplo 404), lanzamos un error personalizado
+                throw new Error("El articulo no se ha encontrado");
+            }
+            // Si todo va bien, parseamos el cuerpo de la respuesta a JSON
+            resolve(response.json());
+        })
+        .then((response) => console.log(response))
+        .catch(error => {
+            // Captura tanto errores de red como los lanzados manualmente en el primer then
+            console.error("Error:", error);
+        });
+    });
+    // Ejecuta una petición GET a la API con el numero de pagina proporcionado 
+}
